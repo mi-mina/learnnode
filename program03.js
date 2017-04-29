a64e7324b8ed1388329243e5337f85cf20d9c5ba
Esperanza <hola@mi-mina.com>
2017-04-29 20:00:56 +0000

    add comment

--- program03.js
+++ program03.js
@@ -1,5 +1,7 @@
 const read = require('fs').readFile;
 
+// readFile is asynchronous, so we muss pass a callback function to it.
+
 read(process.argv[2], (err, data) => {
     if (err) throw err
     console.log(data.toString().split('\n').length - 1);
