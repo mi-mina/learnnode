2f28c1ac74a722a4d5846de4a8ba97860e921fc3
Esperanza <hola@mi-mina.com>
2017-04-30 10:18:36 +0000

    change files names

--- program04.js
+++
@@ -1,8 +0,0 @@
-const readDir = require("fs").readdir;
-const ext = require("path").extname;
-
-readDir(process.argv[2], (err, list) => {
-    if (err) throw err;
-    var filteredList = list.filter(elem => ext(elem) === '.' + process.argv[3])
-    filteredList.forEach(elem => console.log(elem))
-})
